ed642edba038f5a3f9d5ae87bed3e4fa862b298a
Eduardo Guzman <arionangell@gmail.com>
2021-02-03 16:26:42 -0300

    ejercicio4

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\User;
+class LoginController extends AbstractFOSRestController
+{
+     /**
+     * @Route("/login", name="login1")
+     */
+    public function index(Request $request)
+    {
+        return $this->render('login.html.twig' );
+    }
+
+     /**
+     * @Rest\Post("/login_check", name="login")
+     * @Rest\RequestParam(name="email", description="email del usuario", strict=false, nullable=true)
+     * @Rest\RequestParam(name="password", description="password del usuario", strict=false, nullable=true)
+     * @param Request $request
+     * @param ParamFetcherInterface $paramFetcher
+     * @return Response
+     */
+
+        public function login(Request $request, ParamFetcherInterface $paramFetcher){
+             $email = $paramFetcher->get('email');
+             $password = $paramFetcher->get('password');
+             $em = $this->getDoctrine()->getManager();
+             $repositorio= $this->getDoctrine()->getRepository(user::class);
+             $uservalido= $repositorio->findBy([
+                "email" => $email,
+                "pass" => $password
+                                                ]);
+            
+            if (!empty($uservalido)){
+               dd($email);
+                                    }
+           else{
+               
+                    dd($password);
+            }
+            
+            
+            
+             
+       }
+    }
